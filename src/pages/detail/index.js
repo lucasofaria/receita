becc8066c75f30fe9e398204eff8e49555558fbb
lucasofaria <lucas092007@hotmail.com>
2023-04-07 14:09:53 -0300

    Instruções e ingredientes add a pagina de Details

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,11 @@
 import { useLayoutEffect } from 'react';
-import {View, Text, StyleSheet, Pressable} from 'react-native';
+import { View, Text, StyleSheet, Pressable, ScrollView, Image } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, AntDesign, Feather } from '@expo/vector-icons'
+
+import { Ingredients } from '../../components/ingredients'
+import { Instructions } from '../../components/instructions'
 
 export function Detail(){
   const route = useRoute();
@@ -25,15 +28,95 @@ export function Detail(){
   }, [navigation, route.params?.data])
 
   return(
-    <View style={styles.container}>
-      
-    </View>
+    <ScrollView contentContainerStyle={{ paddingBottom: 14 }} style={styles.container} showsVerticalScrollIndicator={false}>
+      <Pressable>
+        <View style={styles.playIcon}>
+          <AntDesign name="playcircleo" size={50} color="#FAFAFA" />
+        </View>
+        <Image 
+          source={{ uri: route.params?.data.cover }}
+          style={styles.cover}
+        />
+      </Pressable>
+
+      <View style={styles.headerDetails}>
+        <View>
+          <Text style={styles.title}>{route.params?.data.name}</Text>
+          <Text style={styles.ingredientsText}>Ingredientes ({route.params?.data.total_ingredients})</Text>
+        </View>
+        <Pressable>
+          <Feather name="share-2" size={24} color="#121212"/>
+        </Pressable>
+      </View>
+
+      {route.params?.data.ingredients.map( (item) => (
+        <Ingredients key={item.id} data={item}/>
+      ))}
+
+      <View style={styles.instructionsArea}>
+        <Text style={styles.instructionsText}>Modo de preparo</Text>
+        <Feather
+          name="arrow-down"
+          size={24}
+          color="#FFF"
+        />
+      </View>
+
+      {route.params?.data.instructions.map((item, index) => (
+        <Instructions key={item.id} data={item} index={index}/>
+      ))}
+
+    </ScrollView>
   )
 } 
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
-    backgroundColor: '#F3F9FF'
+    backgroundColor: '#F3F9FF',
+    paddingTop: 14,
+    paddingEnd: 14,
+    paddingStart: 14
+  },
+  cover:{
+    height: 200,
+    borderRadius: 14,
+    width: '100%'
+  },
+  playIcon:{
+    position: 'absolute',
+    zIndex: 9,
+    top: 0, left: 0, right: 0, bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title:{
+    fontSize: 18,
+    marginTop: 14,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 4
+  },
+  ingredientsText:{
+    marginBottom: 14,
+    fontSize: 16
+  },
+  headerDetails:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 14
+  },
+  instructionsArea:{
+    backgroundColor: "#4cbe6c",
+    flexDirection: 'row',
+    padding: 8,
+    borderRadius: 4,
+    marginBottom: 14
+  },
+  instructionsText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginRight: 8
   }
 })
